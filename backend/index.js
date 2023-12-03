732e439c250edf2ffd7deda9ebe856fc6d44e276
vivek <chimnanivivek14@gmail.com>
2023-12-03 11:54:01 +0530

    Trying to handle client side routing from backend

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const postRoutes = require("./routes/post");
 const conversationRoutes = require("./routes/conversation");
 const messageRoutes = require("./routes/message");
 
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "..", "build")));
 
 app.use("/user", userRoutes);
 app.use("/post", postRoutes);
@@ -35,8 +35,8 @@ app.use("/auth", authRoutes);
 app.use("/conversation", conversationRoutes);
 app.use("/message", messageRoutes);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 app.use((err, req, res, next) => {

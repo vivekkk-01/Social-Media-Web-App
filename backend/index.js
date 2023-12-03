979d45d959409a8043b4dd9e815bd303cea83a1e
vivek <chimnanivivek14@gmail.com>
2023-12-03 20:08:12 +0530

    Added public instead of build

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const postRoutes = require("./routes/post");
 const conversationRoutes = require("./routes/conversation");
 const messageRoutes = require("./routes/message");
 
-app.use(express.static(path.join(__dirname, "..", "frontend", "build")));
+app.use(express.static(path.join(__dirname, "..", "frontend", "public")));
 
 app.use("/user", userRoutes);
 app.use("/post", postRoutes);
@@ -38,7 +38,7 @@ app.use("/message", messageRoutes);
 app.get("/*", (req, res) => {
   console.log("Found a request from frontend...");
   const data = fs.readFileSync(
-    path.join(__dirname, "..", "frontend", "build", "index.html"),
+    path.join(__dirname, "..", "frontend", "public", "index.html"),
     "utf8"
   );
   console.log("File contents:", data);
